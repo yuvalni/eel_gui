e3732b7629da61a8f041ac98fa416d0edc87e15a
yuvalni <original.pr@gmail.com>
2020-02-05 18:54:27 +0200

    socket start

---
+++ socket_client.py
@@ -0,0 +1,9 @@
+import socket
+HOST = 'localhost'
+PORT = 5000
+
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+    s.connect((HOST,PORT))
+    s.sendall(b'Hello World.')
+    data = s.recv(1024)
+print('Recived ', repr(data))
